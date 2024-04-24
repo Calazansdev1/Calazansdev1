71a2d308a0b7f8d584d468a3984b9b5d4af8b46b
Gabriel Gomes Calazans <167626841+Calazansdev1@users.noreply.github.com>
2024-04-24 16:27:44 -0400

    Add files via upload

---
+++ projeto relogio digital/script.js
@@ -0,0 +1,27 @@
+const hoursElement = document.getElementById('hours')
+const minutesElement = document.getElementById('minutes')
+const secondsElement = document.getElementById('seconds')
+
+function newTime(){
+
+const date = new Date();
+
+const hours = date.getHours()
+const minutes = date.getMinutes()
+const seconds = date.getSeconds()
+
+hoursElement.textContent = fixTime(hours)
+minutesElement.textContent = fixTime(minutes)
+secondsElement.textContent = fixTime(seconds)
+}
+
+function fixTime(time){
+    return time < 10 ? '0'+time : time
+}
+
+setInterval(() => {
+    console.log('alo')
+},1000 )
+
+newTime()
+setInterval(newTime, 1000)
